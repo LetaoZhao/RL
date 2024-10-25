333d0911b47b3957b4e7b9b7ba2743af73a57ba6
LetaoZhao <tonyzhao0227@gmail.com>
2024-10-25 15:15:14 +1300

    test

--- pyboy_environment/environments/pokemon/tasks/brock.py
+++ pyboy_environment/environments/pokemon/tasks/brock.py
@@ -39,8 +39,8 @@ class PokemonBrock(PokemonEnvironment):
             # WindowEvent.RELEASE_BUTTON_START,
         ]
 
-        self.count = 0.0
-        self.isMapReach = [0,0,0]
+        # self.count = 0.0
+        # self.isMapReach = [0,0,0]
 
         super().__init__(
             act_freq=act_freq,
@@ -63,11 +63,11 @@ class PokemonBrock(PokemonEnvironment):
 
         # time.sleep(0.1)
 
-        if (self.count < 1000):
-            self.count += 1
-        else:
-            self.count = 0
-            self.isMapReach = [0,0,0]
+        # if (self.count < 1000):
+        #     self.count += 1
+        # else:
+        #     self.count = 0
+        #     self.isMapReach = [0,0,0]
 
         return return_state
     
@@ -95,15 +95,16 @@ class PokemonBrock(PokemonEnvironment):
         # pre_location_x = self.prior_game_stats["location"]["x"]
         # pre_location_y = self.prior_game_stats["location"]["y"]
 
-        if((new_state["location"]["map_id"] == 0) and (self.isMapReach[1] == 0)):
-            print("in")
-            return_score += 1000
-            self.isMapReach[1] = 1
+        # if((new_state["location"]["map_id"] == 0) and (self.isMapReach[1] == 0)):
+        #     print("in")
+        #     return_score += 1000
+        #     self.isMapReach[1] = 1
 
         if (new_state["location"]["map_id"] == 40):
-            return_score += self.distance_to_target_score(new_state,[5,10],10,1)
+            return_score += self.distance_to_target_score(new_state,[5,12],7,1)
 
         if (new_state["location"]["map_id"] == 0):
+            return_score += 1
             return_score += self.distance_to_target_score(new_state,[9,12],6,2)
             
 
