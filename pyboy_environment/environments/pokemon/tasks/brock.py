ed0dde974783aaeebd8dc26d437aae39fb66c416
LetaoZhao <tonyzhao0227@gmail.com>
2024-10-25 15:19:57 +1300

    tes

--- pyboy_environment/environments/pokemon/tasks/brock.py
+++ pyboy_environment/environments/pokemon/tasks/brock.py
@@ -95,17 +95,16 @@ class PokemonBrock(PokemonEnvironment):
         # pre_location_x = self.prior_game_stats["location"]["x"]
         # pre_location_y = self.prior_game_stats["location"]["y"]
 
-        # if((new_state["location"]["map_id"] == 0) and (self.isMapReach[1] == 0)):
-        #     print("in")
-        #     return_score += 1000
-        #     self.isMapReach[1] = 1
+        if((new_state["location"]["map_id"] == 0) and (self.prior_game_stats["location"]["map_id"] == 40)):
+            return_score += 1000
+        if((new_state["location"]["map_id"] == 40) and (self.prior_game_stats["location"]["map_id"] == 0)):
+            return_score -= 1000
 
         if (new_state["location"]["map_id"] == 40):
-            return_score += self.distance_to_target_score(new_state,[5,10],7,1)
+            return_score += self.distance_to_target_score(new_state,[5,10],10,1)
 
         if (new_state["location"]["map_id"] == 0):
-            return_score += 1
-            return_score += self.distance_to_target_score(new_state,[9,12],6,2)
+            return_score += self.distance_to_target_score(new_state,[9,12],6,3)
             
 
         return return_score
